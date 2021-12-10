f7529a1dcac223f0bc79d68a90fd0cafb6523bbc
oskar2517 <12718920+oskar2517@users.noreply.github.com>
2021-12-10 03:20:58 +0100

    add ability to execute and dump binary files

--- src/main/java/me/oskar/peko/Main.java
+++ src/main/java/me/oskar/peko/Main.java
@@ -7,26 +7,42 @@ import me.oskar.peko.vm.VirtualMachine;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            System.out.println("Usage: peko <input>");
+            System.out.println("Usage: peko <input> [--dump]");
             System.exit(1);
         }
 
-        final var code = new String(Files.readAllBytes(Paths.get(args[0])));
+        final var file = args[0];
 
-        final var lexer = new Lexer(code);
-        final var parser = new Parser(lexer);
-        parser.generateAst();
+        if (file.endsWith(".peko")) {
+            final var code = new String(Files.readAllBytes(Paths.get(file)));
 
-        final var compiler = new Compiler(parser.getAst());
-        final var bytecode = compiler.compile();
+            final var lexer = new Lexer(code);
+            final var parser = new Parser(lexer);
+            parser.generateAst();
 
-        final var vm = new VirtualMachine(bytecode);
-        vm.execute();
+            final var compiler = new Compiler(parser.getAst());
+            final var bytecode = compiler.compile();
+
+            if (Arrays.asList(args).contains("--dump")) {
+                Files.write(Path.of("file.bin"), bytecode);
+                System.exit(0);
+            }
+
+            final var vm = new VirtualMachine(bytecode);
+            vm.execute();
+        } else {
+            final var bytecode = Files.readAllBytes(Paths.get(file));
+
+            final var vm = new VirtualMachine(bytecode);
+            vm.execute();
+        }
     }
 }
